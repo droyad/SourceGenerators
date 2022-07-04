c1dadb648b3fda043ed4a57462df9e1e731a9f27
Robert Wagner <robert.wagner@octopus.com>
2022-07-05 09:54:54 +1000

    Simple example

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,26 +1,5 @@
 ﻿using Example.Domain;
 
-var character = new Character("Bob", Class.Sourcerer, 100)
-{
-    LeftHand = new Equipment("Spoon", 40)
-    {
-        Enchantments = { new Enchantment("Healing") }
-    },
-    Inventory =
-    {
-        new Equipment("Carrot", 1),
-        new Equipment("Lamborghini Countach LP5000 Quattrovalvole", 1000)
-    }
-};
-
-
-var readOnly = (IReadOnlyCharacter) character;
-
-//readOnly.Enchantments.Add(new Enchantment("bla"));
-var equipment = readOnly.GetEquipmentWithWearLessThan(50);
-
-foreach(var equipmentItem in equipment)
-    Console.WriteLine(equipmentItem.Name);
-
-Console.ReadLine();
+var type =  Lookup2.Get("Character");
+Console.WriteLine(type);
 
---
+++ SourceGenerator/LookupSourceGenerator.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+
+namespace SourceGenerator;
+
+[Generator]
+public class LookupSourceGenerator : ISourceGenerator
+{
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        //Debugger.Launch();
+
+        context.RegisterForSyntaxNotifications(() => new SyntaxReceiver());
+    }
+
+    public void Execute(GeneratorExecutionContext context)
+    {
+        if (context.SyntaxReceiver is not SyntaxReceiver receiver)
+            throw new Exception("Expected a SyntaxReceiver");
+
+
+        var cases = receiver.AllClasses
+            .Select(c => $"case \"{c.Identifier.ValueText}\": return typeof({c.Identifier.ValueText});");
+
+        var source = $@"
+using Example.Domain;
+
+class Lookup2
+{{
+    public static Type Get(string value)
+    {{
+        switch (value)
+        {{
+            {string.Join(Environment.NewLine, cases)}
+            default:
+                throw new Exception();
+        }}
+    }}
+}}
+";
+
+        context.AddSource("Lookup.g.cs", source);
+    }
+    
+    class SyntaxReceiver : ISyntaxReceiver
+    {
+        public IList<ClassDeclarationSyntax> AllClasses { get; } = new List<ClassDeclarationSyntax>();
+
+        public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+        {
+            if (syntaxNode is ClassDeclarationSyntax cds)
+                AllClasses.Add(cds);
+        }
+    }
+}
